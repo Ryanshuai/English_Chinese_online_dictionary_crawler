d901705f9b90122c1893761d4fd0544cd26cf685
Ryan <1018718155@qq.com>
2019-05-10 02:19:18 +0800

    update word list

--- 05_make_anki_list.py
+++ 05_make_anki_list.py
@@ -6,29 +6,31 @@ from youdict_mem import Youdict_Mem
 
 # ####################################################### build filter list
 tst_txt = 'D:/github_project/make_anki_word_list/word_list/tst.txt'
-cet4_txt = 'D:/github_project/make_anki_word_list/word_list/college_cet_4.txt'
-cet6_txt = 'D:/github_project/make_anki_word_list/word_list/college_cet_6.txt'
-post_txt = 'D:/github_project/make_anki_word_list/word_list/college_考研词汇表.txt'
+college_cet4_txt = 'D:/github_project/make_anki_word_list/word_list/college_cet_4.txt'
+college_cet6_txt = 'D:/github_project/make_anki_word_list/word_list/college_cet_6.txt'
+college_post_txt = 'D:/github_project/make_anki_word_list/word_list/college_考研词汇表.txt'
 toefl_qu_txt = 'D:/github_project/make_anki_word_list/word_list/曲根10000词汇表.txt'
 toefl_red_txt = 'D:/github_project/make_anki_word_list/word_list/TOEFL_红宝书.txt'
-toefl_class = 'D:/github_project/make_anki_word_list/word_list/TOEFL_分类词汇.txt'
-gre3000_txt = 'D:/github_project/make_anki_word_list/word_list/GRE_3000.txt'
+toefl_class_txt = 'D:/github_project/make_anki_word_list/word_list/TOEFL_分类词汇.txt'
+gre_3000_txt = 'D:/github_project/make_anki_word_list/word_list/GRE_3000.txt'
 gre_foot_txt = 'D:/github_project/make_anki_word_list/word_list/GRE_佛脚词.txt'
 gre_red_txt = 'D:/github_project/make_anki_word_list/word_list/GRE红宝书.txt'
-gre_class_txt = 'D:/github_project/make_anki_word_list/word_list/GRE_synonym.txt'
-internal_txt = 'D:/github_project/make_anki_word_list/word_list/internal_word.txt'
+gre_synonym_txt = 'D:/github_project/make_anki_word_list/word_list/GRE_synonym.txt'
+internal_txt = 'D:\github_project\make_anki_word_list\word_list\internal_word.txt'
+all_txt = 'D:\github_project\make_anki_word_list\word_list\all.txt'
 
 input_txt_list = list()
-# input_txt_list.append(tst_txt)
-# input_txt_list.append(cet4_txt)
-# input_txt_list.append(cet6_txt)
-# input_txt_list.append(post_txt)
+input_txt_list.append(tst_txt)
+input_txt_list.append(college_cet4_txt)
+input_txt_list.append(college_cet6_txt)
+input_txt_list.append(college_post_txt)
 input_txt_list.append(toefl_qu_txt)
 input_txt_list.append(toefl_red_txt)
-# input_txt_list.append(gre300_txt)
-# input_txt_list.append(gre_foot_txt)
-# input_txt_list.append(gre_red_txt)
-# input_txt_list.append(internal_txt)
+input_txt_list.append(toefl_class_txt)
+input_txt_list.append(gre_3000_txt)
+input_txt_list.append(gre_foot_txt)
+input_txt_list.append(gre_red_txt)
+input_txt_list.append(gre_synonym_txt)
 
 input_word_set = set()
 for file in input_txt_list:
@@ -38,16 +40,17 @@ for file in input_txt_list:
 
 
 input_txt_list_2 = list()
-# input_txt_list_2.append(tst_txt)
-# input_txt_list_2.append(cet4_txt)
-# input_txt_list_2.append(cet6_txt)
-# input_txt_list_2.append(post_txt)
-# input_txt_list_2.append(toefl_qu_txt)
-# input_txt_list_2.append(toefl_red_txt)
-input_txt_list_2.append(gre3000_txt)
+input_txt_list_2.append(tst_txt)
+input_txt_list_2.append(college_cet4_txt)
+input_txt_list_2.append(college_cet6_txt)
+input_txt_list_2.append(college_post_txt)
+input_txt_list_2.append(toefl_qu_txt)
+input_txt_list_2.append(toefl_red_txt)
+input_txt_list_2.append(toefl_class_txt)
+input_txt_list_2.append(gre_3000_txt)
 input_txt_list_2.append(gre_foot_txt)
-# input_txt_list_2.append(gre_red_txt)
-# input_txt_list.append(internal_txt)
+input_txt_list_2.append(gre_red_txt)
+input_txt_list_2.append(gre_synonym_txt)
 
 input_word_set_2 = set()
 for file in input_txt_list_2:
@@ -99,7 +102,7 @@ print('all: '+str(len(input_word_list)))
 #         f.write('\n')
 #
 
-with open(gre_class_txt, 'r', encoding='utf-8') as f:
+with open(gre_synonym_txt, 'r', encoding='utf-8') as f:
     word_list = f.read().splitlines()
 
 with open('output/GRE_anki_same.txt', 'w', encoding='utf-8') as f:
--- 06_word_in_which.py
+++ 06_word_in_which.py
@@ -1,29 +1,31 @@
 
 # ####################################################### build filter list
 tst_txt = 'D:/github_project/make_anki_word_list/word_list/tst.txt'
-cet4_txt = 'D:/github_project/make_anki_word_list/word_list/college_cet_4.txt'
-cet6_txt = 'D:/github_project/make_anki_word_list/word_list/college_cet_6.txt'
-post_txt = 'D:/github_project/make_anki_word_list/word_list/college_考研词汇表.txt'
+college_cet4_txt = 'D:/github_project/make_anki_word_list/word_list/college_cet_4.txt'
+college_cet6_txt = 'D:/github_project/make_anki_word_list/word_list/college_cet_6.txt'
+college_post_txt = 'D:/github_project/make_anki_word_list/word_list/college_考研词汇表.txt'
 toefl_qu_txt = 'D:/github_project/make_anki_word_list/word_list/曲根10000词汇表.txt'
 toefl_red_txt = 'D:/github_project/make_anki_word_list/word_list/TOEFL_红宝书.txt'
-toefl_class = 'D:/github_project/make_anki_word_list/word_list/TOEFL_分类词汇.txt'
-gre3000_txt = 'D:/github_project/make_anki_word_list/word_list/GRE_3000.txt'
+toefl_class_txt = 'D:/github_project/make_anki_word_list/word_list/TOEFL_分类词汇.txt'
+gre_3000_txt = 'D:/github_project/make_anki_word_list/word_list/GRE_3000.txt'
 gre_foot_txt = 'D:/github_project/make_anki_word_list/word_list/GRE_佛脚词.txt'
 gre_red_txt = 'D:/github_project/make_anki_word_list/word_list/GRE红宝书.txt'
-gre_class_txt = 'D:/github_project/make_anki_word_list/word_list/GRE_synonym.txt'
-internal_txt = 'D:/github_project/make_anki_word_list/word_list/internal_word.txt'
+gre_synonym_txt = 'D:/github_project/make_anki_word_list/word_list/GRE_synonym.txt'
+internal_txt = 'D:\github_project\make_anki_word_list\word_list\internal_word.txt'
+all_txt = 'D:\github_project\make_anki_word_list\word_list\all.txt'
 
 input_txt_list = list()
-input_txt_list.append(cet4_txt)
-input_txt_list.append(cet6_txt)
-input_txt_list.append(post_txt)
+input_txt_list.append(tst_txt)
+input_txt_list.append(college_cet4_txt)
+input_txt_list.append(college_cet6_txt)
+input_txt_list.append(college_post_txt)
 input_txt_list.append(toefl_qu_txt)
 input_txt_list.append(toefl_red_txt)
-input_txt_list.append(toefl_class)
-input_txt_list.append(gre3000_txt)
+input_txt_list.append(toefl_class_txt)
+input_txt_list.append(gre_3000_txt)
 input_txt_list.append(gre_foot_txt)
 input_txt_list.append(gre_red_txt)
-input_txt_list.append(internal_txt)
+input_txt_list.append(gre_synonym_txt)
 
 word = 'demon'
 for file in input_txt_list:
