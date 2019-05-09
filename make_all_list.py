bf3d9002199e361dcb5378c299b547d36c8382e1
Ryan <1018718155@qq.com>
2019-05-10 01:49:35 +0800

    update word list

--- make_all_list.py
+++ make_all_list.py
@@ -7,15 +7,15 @@ import os
 
 # ####################################################### build filter list
 tst_txt = 'D:/github_project/make_anki_word_list/word_list/tst.txt'
-cet4_txt = 'D:/github_project/make_anki_word_list/word_list/4-cet.txt'
-cet6_txt = 'D:/github_project/make_anki_word_list/word_list/6-cet.txt'
-post_txt = 'D:/github_project/make_anki_word_list/word_list/考研词汇表.txt'
+cet4_txt = 'D:/github_project/make_anki_word_list/word_list/college_cet_4.txt'
+cet6_txt = 'D:/github_project/make_anki_word_list/word_list/college_cet_6.txt'
+post_txt = 'D:/github_project/make_anki_word_list/word_list/college_考研词汇表.txt'
 toefl_qu_txt = 'D:/github_project/make_anki_word_list/word_list/曲根10000词汇表.txt'
-toefl_red_txt = 'D:/github_project/make_anki_word_list/word_list/托福红宝书.txt'
-toefl_class = 'D:/github_project/make_anki_word_list/word_list/分类词汇.txt'
-gre3000_txt = 'D:/github_project/make_anki_word_list/word_list/3000.txt'
-gre_foot_txt = 'D:/github_project/make_anki_word_list/word_list/佛脚词.txt'
-gre_red_txt = 'D:/github_project/make_anki_word_list/word_list/gre红宝书.txt'
+toefl_red_txt = 'D:/github_project/make_anki_word_list/word_list/TOEFL_红宝书.txt'
+toefl_class = 'D:/github_project/make_anki_word_list/word_list/TOEFL_分类词汇.txt'
+gre3000_txt = 'D:/github_project/make_anki_word_list/word_list/GRE_3000.txt'
+gre_foot_txt = 'D:/github_project/make_anki_word_list/word_list/GRE_佛脚词.txt'
+gre_red_txt = 'D:/github_project/make_anki_word_list/word_list/GRE红宝书.txt'
 gre_class_txt = 'D:/github_project/make_anki_word_list/word_list/GRE_synonym.txt'
 internal_txt = 'D:/github_project/make_anki_word_list/word_list/internal_word.txt'
 
