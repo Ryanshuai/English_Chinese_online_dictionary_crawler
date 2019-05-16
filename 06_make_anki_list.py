6550ce70af5ec844d203cee3a3454b52becd89de
Ryan <1018718155@qq.com>
2019-05-16 21:22:27 +0800

    venn OK

--- 06_make_anki_list.py
+++ 06_make_anki_list.py
@@ -1,21 +1,17 @@
-import yaml
-from similar_word import Distance_Similar
-from word_root import Youdict_Root, Cigencizhui_Root
-from youdict_mem import Youdict_Mem
+from word_root import Cigencizhui_Root
 
 
-# ####################################################### build filter list
 tst_txt = 'D:/github_project/make_anki_word_list/word_list/tst.txt'
 high_school_txt = 'D:/github_project/make_anki_word_list/word_list/high_school.txt'
 college_cet4_txt = 'D:/github_project/make_anki_word_list/word_list/college_cet_4.txt'
 college_cet6_txt = 'D:/github_project/make_anki_word_list/word_list/college_cet_6.txt'
 college_post_txt = 'D:/github_project/make_anki_word_list/word_list/college_考研词汇表.txt'
-toefl_qu_txt = 'D:/github_project/make_anki_word_list/word_list/曲根10000词汇表.txt'
+toefl_qu_txt = 'D:/github_project/make_anki_word_list/word_list/qu_10000.txt'
 toefl_red_txt = 'D:/github_project/make_anki_word_list/word_list/TOEFL_红宝书.txt'
 toefl_class_txt = 'D:/github_project/make_anki_word_list/word_list/TOEFL_分类词汇.txt'
 gre_3000_txt = 'D:/github_project/make_anki_word_list/word_list/GRE_3000.txt'
-gre_foot_txt = 'D:/github_project/make_anki_word_list/word_list/GRE_佛脚词.txt'
-gre_red_txt = 'D:/github_project/make_anki_word_list/word_list/GRE红宝书.txt'
+gre_foot_txt = 'D:/github_project/make_anki_word_list/word_list/GRE_foot.txt'
+gre_red_txt = 'D:/github_project/make_anki_word_list/word_list/GRE_red.txt'
 gre_synonym_txt = 'D:/github_project/make_anki_word_list/word_list/GRE_synonym.txt'
 internal_txt = 'D:/github_project/make_anki_word_list/word_list/internal_word.txt'
 all_txt = 'D:/github_project/make_anki_word_list/word_list/all.txt'
@@ -26,9 +22,9 @@ input_txt_list = list()
 # input_txt_list.append(college_cet4_txt)
 # input_txt_list.append(college_cet6_txt)
 # input_txt_list.append(college_post_txt)
-# input_txt_list.append(toefl_red_txt)
+input_txt_list.append(toefl_red_txt)
 # input_txt_list.append(toefl_class_txt)
-# input_txt_list.append(toefl_qu_txt)
+input_txt_list.append(toefl_qu_txt)
 # input_txt_list.append(gre_3000_txt)
 # input_txt_list.append(gre_foot_txt)
 # input_txt_list.append(gre_red_txt)
@@ -40,6 +36,8 @@ for file in input_txt_list:
         word_list = f.read().splitlines()
         input_word_set |= set(word_list)
 print('len of input_word_set: ', len(input_word_set))
+print('input_txt_list: ', list(map(lambda x: x.split('/')[-1][:-4], input_txt_list)))
+print('-----------------------------------------------------')
 
 input_txt_list_2 = list()
 # input_txt_list_2.append(tst_txt)
@@ -50,10 +48,10 @@ input_txt_list_2 = list()
 # input_txt_list_2.append(toefl_qu_txt)
 # input_txt_list_2.append(toefl_red_txt)
 # input_txt_list_2.append(toefl_class_txt)
-# input_txt_list.append(gre_3000_txt)
+input_txt_list_2.append(gre_3000_txt)
 # input_txt_list_2.append(gre_foot_txt)
 # input_txt_list_2.append(gre_red_txt)
-input_txt_list_2.append(gre_synonym_txt)
+# input_txt_list_2.append(gre_synonym_txt)
 # input_txt_list_2.append(internal_txt)
 
 input_word_set_2 = set()
@@ -62,54 +60,70 @@ for file in input_txt_list_2:
         word_list = f.read().splitlines()
         input_word_set_2 |= set(word_list)
 print('len of input_word_set_2: ', len(input_word_set_2))
+print('input_txt_list_2: ', list(map(lambda x: x.split('/')[-1][:-4], input_txt_list_2)))
+print('-----------------------------------------------------')
+
+final_word_list = list(input_word_set_2 & input_word_set)
+print('input_word_set_2-input_word_set', len(final_word_list))
 
 
-input_word_list = list(input_word_set_2-input_word_set)
-print('input_word_set_2-input_word_set', len(input_word_set_2-input_word_set))
-
-# ####################################################### build word list in root
-cigencizhui_root = Cigencizhui_Root()
-youdict_root = Youdict_Root()
-youdict_mem = Youdict_Mem()
-edit_distance = Distance_Similar()
-
-
-# with open('pure_word_list.txt', 'w', encoding='utf-8') as f:
-#     ordered_input_word_list = cigencizhui_root.put_word_list_in_order(input_word_list)
-#     for word in ordered_input_word_list:
-#         print(word)
-#         f.write(word)
-#         f.write('\n')
-#
-
-with open('output/anki_word_image_list.txt', 'w', encoding='utf-8') as f:
-    # ordered_input_word_list = cigencizhui_root.put_word_list_in_order(input_word_list)
-    # print('ordered_input_word_list: ', len(ordered_input_word_list))
-    print('ordered_input_word_list: ', len(input_word_list))
-    for word in word_list:
-        word = word.strip()
-        line = word
-        for i in range(1, 11):
-            line += "\\<img src=\"{}.jpg\" />".format(word+str(i))
-        f.write(line)
-        f.write('\n')
+def make_pure_word_list():
+    cigencizhui_root = Cigencizhui_Root()
+    with open('pure_word_list.txt', 'w', encoding='utf-8') as f:
+        ordered_input_word_list = cigencizhui_root.put_word_list_in_order(final_word_list)
+        for word in ordered_input_word_list:
+            print(word)
+            f.write(word)
+            f.write('\n')
 
 
-# with open(gre_synonym_txt, 'r', encoding='utf-8') as f:
-#     word_list = f.read().splitlines()
-#
-# with open('output/GRE_anki_same.txt', 'w', encoding='utf-8') as f:
-#     for word in word_list:
-#         word = word.strip()
-#         if len(word) == 0:
-#             continue
-#         print(word)
-#         if '\u4e00' <= word[0] <= '\u9fff':
-#             now_class = word
-#         else:
-#             line = word
-#             for i in range(1, 11):
-#                 line += "\\<img src=\"{}.jpg\" />".format(word + str(i))
-#             line += "\\"+now_class
-#             f.write(line)
-#             f.write('\n')
+def make_anki_word_image_list():
+    cigencizhui_root = Cigencizhui_Root()
+    with open('output/anki_word_image_list.txt', 'w', encoding='utf-8') as f:
+        # final_word_list = cigencizhui_root.put_word_list_in_order(final_word_list)
+        print('ordered_input_word_list: ', len(final_word_list))
+        for word in final_word_list:
+            word = word.strip()
+            line = word
+            for i in range(1, 11):
+                line += "\\<img src=\"{}.jpg\" />".format(word+str(i))
+            f.write(line)
+            f.write('\n')
+
+
+def make_gre_synonym_image_list():
+    with open(gre_synonym_txt, 'r', encoding='utf-8') as f:
+        word_list = f.read().splitlines()
+
+    with open('output/GRE_anki_same.txt', 'w', encoding='utf-8') as f:
+        for word in word_list:
+            word = word.strip()
+            if len(word) == 0:
+                continue
+            print(word)
+            if '\u4e00' <= word[0] <= '\u9fff':
+                now_class = word
+            else:
+                line = word
+                for i in range(1, 11):
+                    line += "\\<img src=\"{}.jpg\" />".format(word + str(i))
+                line += "\\"+now_class
+                f.write(line)
+                f.write('\n')
+
+
+def make_anki_delete_list():
+    with open('output/anki_delete_list.txt', 'w', encoding='utf-8') as f:
+        for word in final_word_list:
+            word = word.strip()
+            line = word
+            line += '\\_____'
+            f.write(line)
+            f.write('\n')
+
+
+if __name__ == '__main__':
+    # make_anki_delete_list()
+    make_anki_word_image_list()
+
+
---
+++ 08_analyze_word_list.py
@@ -0,0 +1,65 @@
+import venn
+
+
+high_school_txt = 'D:/github_project/make_anki_word_list/word_list/high_school.txt'
+college_cet4_txt = 'D:/github_project/make_anki_word_list/word_list/college_cet_4.txt'
+college_cet6_txt = 'D:/github_project/make_anki_word_list/word_list/college_cet_6.txt'
+college_post_txt = 'D:/github_project/make_anki_word_list/word_list/college_考研词汇表.txt'
+toefl_qu_txt = 'D:/github_project/make_anki_word_list/word_list/qu_10000.txt'
+toefl_red_txt = 'D:/github_project/make_anki_word_list/word_list/TOEFL_红宝书.txt'
+toefl_class_txt = 'D:/github_project/make_anki_word_list/word_list/TOEFL_分类词汇.txt'
+gre_3000_txt = 'D:/github_project/make_anki_word_list/word_list/GRE_3000.txt'
+gre_foot_txt = 'D:/github_project/make_anki_word_list/word_list/GRE_foot.txt'
+gre_red_txt = 'D:/github_project/make_anki_word_list/word_list/GRE_red.txt'
+gre_synonym_txt = 'D:/github_project/make_anki_word_list/word_list/GRE_synonym.txt'
+internal_txt = 'D:/github_project/make_anki_word_list/word_list/internal_word.txt'
+all_txt = 'D:/github_project/make_anki_word_list/word_list/all.txt'
+
+with open(high_school_txt, 'r', encoding='utf-8') as f:
+    high_school_set = set(f.read().splitlines())
+with open(college_cet4_txt, 'r', encoding='utf-8') as f:
+    college_cet4_set = set(f.read().splitlines())
+with open(college_cet6_txt, 'r', encoding='utf-8') as f:
+    college_cet6_set = set(f.read().splitlines())
+with open(college_post_txt, 'r', encoding='utf-8') as f:
+    college_post_set = set(f.read().splitlines())
+with open(toefl_qu_txt, 'r', encoding='utf-8') as f:
+    toefl_qu_set = set(f.read().splitlines())
+with open(toefl_red_txt, 'r', encoding='utf-8') as f:
+    toefl_red_set = set(f.read().splitlines())
+with open(toefl_class_txt, 'r', encoding='utf-8') as f:
+    toefl_class_set = set(f.read().splitlines())
+with open(gre_3000_txt, 'r', encoding='utf-8') as f:
+    gre_3000_set = set(f.read().splitlines())
+with open(gre_foot_txt, 'r', encoding='utf-8') as f:
+    gre_foot_set = set(f.read().splitlines())
+with open(gre_red_txt, 'r', encoding='utf-8') as f:
+    gre_red_set = set(f.read().splitlines())
+with open(gre_synonym_txt, 'r', encoding='utf-8') as f:
+    gre_synonym_set = set(f.read().splitlines())
+college_set = college_cet4_set | college_cet6_set | college_post_set
+toefl_set = toefl_qu_set | toefl_red_set
+gre_set = gre_foot_set | gre_3000_set
+
+# set_list = [college_set, toefl_set, gre_set, gre_red_set]
+# name_list = ['c', 't', 'g', 'gr']
+set_list = [gre_3000_set, gre_foot_set, gre_red_set, toefl_set]
+name_list = ['3000', 'foot', 'gre_red', 'toefl']
+
+
+num = len(set_list)
+assert 2 <= num <= 6
+if num == 2:
+    venn_venn = venn.venn2
+if num == 3:
+    venn_venn = venn.venn3
+if num == 4:
+    venn_venn = venn.venn4
+if num == 5:
+    venn_venn = venn.venn5
+if num == 6:
+    venn_venn = venn.venn6
+
+labels = venn.get_labels(set_list, fill=['number', 'logic'])
+fig, ax = venn_venn(labels, names=name_list)
+fig.show()
