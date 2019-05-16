812ea458bfd470a3b3a97d2cb88b89135265d1ce
Ryan <1018718155@qq.com>
2019-05-16 17:33:15 +0800

    add analyze.py

--- analyze_word_list.py
+++ analyze_word_list.py
@@ -1,6 +1,7 @@
+import pylab as plt
+from matplotlib_venn import venn3, venn3_circles
 
-# ####################################################### build filter list
-tst_txt = 'D:/github_project/make_anki_word_list/word_list/tst.txt'
+
 high_school_txt = 'D:/github_project/make_anki_word_list/word_list/high_school.txt'
 college_cet4_txt = 'D:/github_project/make_anki_word_list/word_list/college_cet_4.txt'
 college_cet6_txt = 'D:/github_project/make_anki_word_list/word_list/college_cet_6.txt'
@@ -16,38 +17,27 @@ internal_txt = 'D:/github_project/make_anki_word_list/word_list/internal_word.tx
 all_txt = 'D:/github_project/make_anki_word_list/word_list/all.txt'
 
 input_txt_list = list()
-input_txt_list.append(tst_txt)
-input_txt_list.append(high_school_txt)
-input_txt_list.append(college_cet4_txt)
-input_txt_list.append(college_cet6_txt)
-input_txt_list.append(college_post_txt)
-input_txt_list.append(toefl_qu_txt)
-input_txt_list.append(toefl_red_txt)
-input_txt_list.append(toefl_class_txt)
+# input_txt_list.append(high_school_txt)
+# input_txt_list.append(college_cet4_txt)
+# input_txt_list.append(college_cet6_txt)
+# input_txt_list.append(college_post_txt)
+# input_txt_list.append(toefl_qu_txt)
+# input_txt_list.append(toefl_red_txt)
+# input_txt_list.append(toefl_class_txt)
 input_txt_list.append(gre_3000_txt)
-input_txt_list.append(gre_foot_txt)
+# input_txt_list.append(gre_foot_txt)
 input_txt_list.append(gre_red_txt)
 input_txt_list.append(gre_synonym_txt)
 
-word = 'ill-bred'
+set_list = list()
+name_list = list()
 for file in input_txt_list:
     with open(file, 'r', encoding='utf-8') as f:
         word_list = f.read().splitlines()
-    if word in word_list:
-        print(word, '\tis in list: ',file)
+        set_list.append(set(word_list))
+        name = file.split('/')[-1][:-4]
+        name_list.append(name)
 
-print('=====================================================================')
-root = 'nug'
-with open(all_txt, 'r', encoding='utf-8') as f:
-    word_list = f.read().splitlines()
-for word in word_list:
-    if root in word:
-        print(word, '\thas root:\t', root)
-
-print('=====================================================================')
-suffix = 'illo'
-with open(all_txt, 'r', encoding='utf-8') as f:
-    word_list = f.read().splitlines()
-for word in word_list:
-    if word.endswith(suffix):
-        print(word, '\thas suffix:\t', suffix)
+venn3(set_list, tuple(name_list))
+# venn3(set_list, set_labels = ('A', 'B', 'C'))
+plt.show()
