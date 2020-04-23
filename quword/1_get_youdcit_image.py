a4f6edc5534bf3d25016cb5914e66bb0c0387835
Shuai_Yan <1018718155@qq.com>
2020-04-23 17:42:08 +0800

    add image

--- quword/1_get_youdcit_image.py
+++ quword/1_get_youdcit_image.py
@@ -7,11 +7,16 @@ import os
 import time
 import random
 
+from PIL import ImageFile
+
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+
 
 def save_word_image_to_dir(name, save_path):
-    url = 'http://www.youdict.com/images/words/' + name + '.jpg'
+    url = 'https://www.quword.com/images/words/' + name + '.jpg'
+    response = requests.get(url)
     try:
-        response = requests.get(url)
+        # response = requests.get(url)
         image = Image.open(BytesIO(response.content))
     except:
         return False
@@ -27,8 +32,10 @@ if __name__ == '__main__':
 
     save_to_dir = './quword_images/'
 
-    # pool = ThreadPoolExecutor(2)
-    for word in all_words_list:
+    pool = ThreadPoolExecutor(4)
+    form_num = 3321
+    for i, word in enumerate(all_words_list[form_num:]):
+        print(i + form_num)
         word = word.strip()
         for i in range(1, 11):
             name = word + str(i)
@@ -38,5 +45,4 @@ if __name__ == '__main__':
                 is_found = save_word_image_to_dir(name, save_path)
                 if not is_found:
                     break
-                time.sleep(random.randint(1, 5))
-                # a = pool.submit(save_word_image_to_dir, name, save_path)
+                time.sleep(random.random() * 0.2)
